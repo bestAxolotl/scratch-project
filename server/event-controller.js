17dc8abac1ffdf6d45d73888186aa77260699fbd
Carolyn Harrold <carolyn.harrold@gmail.com>
2019-01-18 13:00:24 -0800

    untested routes created

---
+++ server/event-controller.js
@@ -0,0 +1,17 @@
+const db = require('./database');
+
+const eventController = {};
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = eventController;
--- server/index.js
+++ server/index.js
@@ -4,15 +4,60 @@ const http = require('http');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors')
-
 const server = http.createServer(app);
+const PORT = 8000;
+const db = require('./database');
+const eventController = require('./event-controller');
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors())
 
-const PORT = 3000;
+
+//---------- Welcome / Search Page Routes -------
+
+//Search activities route
+app.get('/activities', eventController.activitySearch);
+
+//Activity detail display route, accessable once search is populated
+app.get('/activity/:id', eventController.activityDetailPageDisplay);
+
+
+
+//---------- Activity Detail Page Routes ---------------
+
+//Confirm participation + save to profile route
+app.post('/activity/:id/confirm', eventController.confirmParticipation);
+
+//Save event as a maybe to profile route
+app.post('/activity/:id/maybe', eventController.saveAsMaybe);
+
+//Submit chat text route
+app.post('/activity/:id/submit', eventController.submitChatText);
+
+//View event creator and attendee profile route
+app.get('/profile/:id', eventController.viewProfile);
+
+
+
+//---------- Create Activity Page Route -----------------
+
+//Create activity button route
+app.post('/createactivity', eventController.createActivity);
+
+
+
+//---------- Login Page Routes --------------------------
+
+//Login button route
+app.post('/auth/login', eventController.login);
+
+//Create account button route
+app.post('/auth/create', eventController.createAccount);
+
+
 
 server.listen(PORT, () => {
   console.log(`Connected and listening on ${PORT}`);
